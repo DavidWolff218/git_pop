6339c9e25838c501f303d0c4ef406580da69a477
doodleman218 <dcwolff218@gmail.com>
2024-09-04 11:30:37 -0500

    bubble click animation complete

--- src/Components/Bubble.tsx
+++ src/Components/Bubble.tsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Bubble = () => {
+  const [level, setLevel] = useState(0);
+  const [levelColor, setLevelColor] = useState("bg-grey-start");
+  const [expanded, setExpanded] = useState(false)
 
-  const [level, setLevel] = useState(0)
-  const [levelColor, setLevelColor] = useState('bg-grey-start')
-
-  const handleClick = () => {
-    if(level < 4){
-      setLevel(prev => {
-        const newLevel = prev + 1
-        setLevelColor(`bg-green-${newLevel}`)
-        return newLevel
-      }
-      )
+  const handleMouseOver = () => {
+    if (level < 4) {
+      setLevel((prev) => {
+        const newLevel = prev + 1;
+        setLevelColor(`bg-green-${newLevel}`);
+        setExpanded(true)
+        setTimeout(() => {setExpanded(false)}, 250)
+        return newLevel;
+      });
     }
-  }
+  };
 
-  return ( <>
-  <div onClick={handleClick} className={`size-2.5 ${levelColor} rounded-sm m-0.5`} />
-  </> );
-}
+  return (
+    <>
+      <div
+        onClick={handleMouseOver}
+        className={`size-2.5 ${levelColor} rounded-sm my-0.5 mx-[1px] transition-transform duration-100 ease-in-out ${expanded ? "scale-110" : ""}`}
+      />
+    </>
+  );
+};
 
-export default Bubble ;
+export default Bubble;
