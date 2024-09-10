bb4255ab8e8cb9d7f21a9de50ee45d42e558daae
doodleman218 <dcwolff218@gmail.com>
2024-09-10 14:19:50 -0500

    added toast notfication and text

--- src/Components/Bubble.tsx
+++ src/Components/Bubble.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+
+
 type bubbleProps = {
   handleUpdateCount: () => void;
 }
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,17 +1,36 @@
 import BubbleContainer from "./BubbleContainer";
 import RepoBoxes from "./RepoBoxes";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const Home = () => {
+  useEffect(() => {
+    toast(
+      `Feeling the pressure to keep up with your GitHub contributions? 
+      We know it can be tough to code every day and earn those green squares. But don't worry, here  you can fill in the squares and enjoy the instant satisfaction of looking super productive. Pretend you've done all the coding and bask in the green glow!`,
+      {
+        position: "top-center",
+        autoClose: 10000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "dark",
+        // transition: Bounce,
+      }
+    );
+  }, []);
+
   return (
     <div className="h-screen flex justify-center mt-10">
       <div className="items-start flex flex-col">
-      <div className="flex items-center">
-        <div className="w-[200px] h-[200px] rounded-full border-[2px] border-grey-line" />
-        <div className="ml-5">
-          <RepoBoxes />
+        <div className="flex items-center">
+          <div className="w-[200px] h-[200px] rounded-full border-[2px] border-grey-line" />
+          <div className="ml-5">
+            <RepoBoxes />
+          </div>
         </div>
-      </div>
-      <BubbleContainer />
+        <BubbleContainer />
       </div>
     </div>
   );
