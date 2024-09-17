c0cd0d761aca1d4223d467fe6317e83922f6c7ed
doodleman218 <dcwolff218@gmail.com>
2024-09-17 10:35:19 -0500

    cleaning up modal, changed text, added actual button

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
+
 type ModalProps = {
   handleModalClick: () => void;
 };
 
 const Modal = ({ handleModalClick }: ModalProps) => {
+
+  // const [expanded, setExpanded]
   return (
     <>
       <div className="fixed inset-0 flex items-center justify-center z-1000 bg-slate-800/75">
-        <div className="bg-teal-200 h-1/4 w-1/4">
-          <button onClick={handleModalClick}>X</button>
-          <h2>
-            Feeling the pressure to keep up with your GitHub contributions? We
-            know it can be tough to code every day and earn those green squares.
-            But don't worry, here you can fill in the squares and enjoy the
-            instant satisfaction of looking super productive. Pretend you've
-            done all the coding and bask in the green glow!`,
-          </h2>
+        <div className="bg-green-2/50 px-8 rounded-lg p-4 border-2">
+          
+          <div className={'flex flex-col items-center justify-center'}>
+            <h2 className="text-xl text-center py-2">
+              Struggling to keep up with your github contributions?
+            </h2>
+            <h2 className="text-xl text-center pb-4">
+              We know it’s tough to code daily and hit those green squares, but here,< br/> you can fill them in and feel instantly productive.
+            </h2>
+            <h2 className="text-2xl text-center pb-2">
+              Pretend you've done all the coding and bask in the green glow!
+            </h2>
+            <button onClick={handleModalClick} className="p-2 text-xl bg-green-3 hover:bg-green-4 my-2 border-2 rounded ">Git Poppin!</button>
+          </div>
         </div>
       </div>
     </>
